6b407722967499e66990cabbed0543a9be956b4f
Robin Collins <Robin.F.Collins@outlook.com>
2023-09-27 01:06:48 +0930

    .

--- src/scrollTo-TopBottom/userScript.js
+++ src/scrollTo-TopBottom/userScript.js
@@ -1,33 +1,35 @@
 // ==UserScript==
-// @name         Scroll To Top & Bottom
+// @name         Scroll Arrows
 // @namespace    http://tampermonkey.net/
 // @version      0.9
-// @description  Adds up and down arrows with toggle colors
-// @author       TheBloke-vicuna-v1.5-16k-13b-q5_k_s.gguf and gpt4
-// @match        *://*/*
+// @description  Customizable arrows to scroll page
+// @author       You
+// @match        http://*/*
+// @icon         https://www.google.com/s2/favicons?domain=tampermonkey.net
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
   
-    // Add SVG definitions
+    // SVG definitions
     var svgContainer = document.createElement('div');
     svgContainer.innerHTML = `
-      <svg aria-hidden="true" style="position: absolute; width: 0; height: 0; overflow: hidden;" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-          <defs>
-              <symbol id="icon-arrow-circle-down" viewBox="0 0 24 28">
-                  <path d="M20.062 14.016c0-0.266-0.094-0.516-0.281-0.703l-1.422-1.422c-0.187-0.187-0.438-0.281-0.703-0.281s-0.516 0.094-0.703 0.281l-2.953 2.953v-7.844c0-0.547-0.453-1-1-1h-2c-0.547 0-1 0.453-1 1v7.844l-2.953-2.953c-0.187-0.187-0.438-0.297-0.703-0.297s-0.516 0.109-0.703 0.297l-1.422 1.422c-0.187 0.187-0.281 0.438-0.281 0.703s0.094 0.516 0.281 0.703l7.078 7.078c0.187 0.187 0.438 0.281 0.703 0.281s0.516-0.094 0.703-0.281l7.078-7.078c0.187-0.187 0.281-0.438 0.281-0.703zM24 14c0 6.625-5.375 12-12 12s-12-5.375-12-12 5.375-12 12-12 12 5.375 12 12z"></path>
-              </symbol>
-              <symbol id="icon-arrow-circle-up" viewBox="0 0 24 28">
-                  <path d="M20.062 13.984c0-0.266-0.094-0.516-0.281-0.703l-7.078-7.078c-0.187-0.187-0.438-0.281-0.703-0.281s-0.516 0.094-0.703 0.281l-7.078 7.078c-0.187 0.187-0.281 0.438-0.281 0.703s0.094 0.516 0.281 0.703l1.422 1.422c0.187 0.187 0.438 0.281 0.703 0.281s0.516-0.094 0.703-0.281l2.953-2.953v7.844c0 0.547 0.453 1 1 1h2c0.547 0 1-0.453 1-1v-7.844l2.953 2.953c0.187 0.187 0.438 0.297 0.703 0.297s0.516-0.109 0.703-0.297l1.422-1.422c0.187-0.187 0.281-0.438 0.281-0.703zM24 14c0 6.625-5.375 12-12 12s-12-5.375-12-12 5.375-12 12-12 12 5.375 12 12z"></path>
-              </symbol>
-          </defs>
-      </svg>`;
-    document.body.insertBefore(svgContainer, document.body.childNodes[0]);
+      <svg style="display: none">
+        <symbol id="arrow-up" viewBox="0 0 24 24">
+          <path d="M11.7 7.7L10.3 6.3 5.9 10.7 5.1 10 4 8.9l6-6 6 6-1.1 1.1-0.8-0.8-4.4 4.4z"/>
+        </symbol>  
+        <symbol id="arrow-down" viewBox="0 0 24 24">
+          <path d="M12.3 16.3L13.7 17.7 18.1 13.3 19 12l1.1 1.1-6 6-6-6 1.1-1.1 0.8 0.8 4.4-4.4z"/>
+        </symbol>
+      </svg>
+    `;
+    document.body.insertBefore(svgContainer, document.body.firstChild);
   
-    // Colors
-    let arrowColor = 'black';
+    // Customizable variables
+    let arrowSize = 40; // Pixel size of arrows
+    let arrowOffset = 50; // Pixels from right edge
+    let arrowColor = 'black'; 
     let borderColor;
     if(arrowColor === 'black') {
       borderColor = 'white';
@@ -35,95 +37,90 @@
       borderColor = 'black';
     }
   
-    // Create elements
+    // Create arrow elements
     let svgns = "http://www.w3.org/2000/svg";
-    let svgElemUp = document.createElementNS(svgns, 'svg');
-    let svgElemDown = document.createElementNS(svgns, 'svg');
     let arrowUp = document.createElement('div');
-    let arrowDown = document.createElement('div');
+    let arrowDown = document.createElement('div');  
   
-    // Style arrows
+    // Add SVG and styles
+    let svgUp = document.createElementNS(svgns, 'svg');
+    let svgDown = document.createElementNS(svgns, 'svg');
+    svgUp.classList.add('icon', 'icon-arrow-up');
+    svgDown.classList.add('icon', 'icon-arrow-down');
+    svgUp.style.fill = arrowColor;
+    svgDown.style.fill = arrowColor;
+  
+    let useUp = document.createElementNS(svgns, 'use');
+    let useDown = document.createElementNS(svgns, 'use');
+    useUp.setAttributeNS('http://www.w3.org/1999/xlink','href','#arrow-up');
+    useDown.setAttributeNS('http://www.w3.org/1999/xlink','href','#arrow-down');
+    svgUp.appendChild(useUp);
+    svgDown.appendChild(useDown);
+  
     arrowUp.style.border = '2px solid';
     arrowDown.style.border = '2px solid';
     arrowUp.style.borderColor = borderColor;
     arrowDown.style.borderColor = borderColor;
   
-    // Add SVG and styles
-    svgElemUp.setAttribute('class', 'icon icon-arrow-circle-up');
-    svgElemDown.setAttribute('class', 'icon icon-arrow-circle-down');
-    svgElemUp.style.fill = arrowColor;
-    svgElemDown.style.fill = arrowColor;
-  
-    // Append SVG to arrows
-    let useElemUp = document.createElementNS(svgns, 'use');
-    let useElemDown = document.createElementNS(svgns, 'use');
-    useElemUp.setAttributeNS('http://www.w3.org/1999/xlink','href','#icon-arrow-circle-up');
-    useElemDown.setAttributeNS('http://www.w3.org/1999/xlink','href','#icon-arrow-circle-down');
-    svgElemUp.appendChild(useElemUp);
-    svgElemDown.appendChild(useElemDown);
-    arrowUp.appendChild(svgElemUp);
-    arrowDown.appendChild(svgElemDown);
-  
-    // Position arrows
+    // Size and position arrows
+    arrowUp.style.width = `${arrowSize}px`;
+    arrowUp.style.height = `${arrowSize}px`;
     arrowUp.style.position = 'fixed';
-    arrowUp.style.right = '50px';
+    arrowUp.style.right = `${arrowOffset}px`;
     arrowUp.style.top = '50%';
-    arrowUp.style.transform = 'translateY(-100%)';
+    arrowUp.style.transform = 'translateY(-50%)';
     arrowUp.style.zIndex = '999999';
   
-    arrowDown.style.position = 'fixed';  
-    arrowDown.style.right = '50px';
+    arrowDown.style.width = `${arrowSize}px`;
+    arrowDown.style.height = `${arrowSize}px`; 
+    arrowDown.style.position = 'fixed';
+    arrowDown.style.right = `${arrowOffset}px`;
     arrowDown.style.top = '50%';
-    arrowDown.style.transform = 'translateY(100%)';
+    arrowDown.style.transform = 'translateY(50%)';
     arrowDown.style.zIndex = '999999';
   
     // Scroll functions
-    var scrollToTop = function() {
-      window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-      });
+    var scrollToTop = () => {
+      window.scrollTo({top: 0, behavior: 'smooth'});
+    };
+    var scrollToBottom = () => {
+      window.scrollTo({top: document.body.scrollHeight, behavior: 'smooth'});
     };
   
-    var scrollToBottom = function() {
-      window.scrollTo({
-        top: document.body.scrollHeight,
-        behavior: 'smooth'
-      });
-    };
-  
-    // Add event listeners
+    // Add arrows and events
     document.body.appendChild(arrowUp);
     document.body.appendChild(arrowDown);
+    arrowUp.appendChild(svgUp);
+    arrowDown.appendChild(svgDown);
     arrowUp.addEventListener('click', scrollToTop);
     arrowDown.addEventListener('click', scrollToBottom);
   
     // Toggle button
-    let flipButton = document.createElement('button');
-    flipButton.textContent = 'Flip Colors';
-    flipButton.style.position = 'fixed';
-    flipButton.style.right = '32px';
-    flipButton.style.top = '50%'; 
-    flipButton.style.transform = 'translateY(-50%)';
-    flipButton.style.zIndex = '99999';
+    let toggleBtn = document.createElement('button');
+    toggleBtn.textContent = 'Toggle Color';
+    toggleBtn.style.position = 'fixed';
+    toggleBtn.style.right = `${arrowOffset}px`;
+    toggleBtn.style.top = '50%';
+    toggleBtn.style.transform = 'translate(-50%, -50%)';
+    toggleBtn.style.zIndex = '99999';
   
-    const toggleArrowColor = () => {
+    const toggleColor = () => {
       if(arrowColor === 'black') {
         arrowColor = 'white';
         borderColor = 'black';
       } else {
         arrowColor = 'black';
-        borderColor = 'white';
+        borderColor = 'white';  
       }
       
-      svgElemUp.style.fill = arrowColor;
-      svgElemDown.style.fill = arrowColor;
+      svgUp.style.fill = arrowColor; 
+      svgDown.style.fill = arrowColor;
       
       arrowUp.style.borderColor = borderColor;
       arrowDown.style.borderColor = borderColor;
-    }
+    };
   
-    flipButton.addEventListener('click', toggleArrowColor);
-    document.body.appendChild(flipButton);
+    toggleBtn.addEventListener('click', toggleColor);
+    document.body.appendChild(toggleBtn);
   
   })();
